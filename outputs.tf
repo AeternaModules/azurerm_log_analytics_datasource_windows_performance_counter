574cee75de95f83f1db9e526268c62d873e64d78
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:48:50 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,33 +1,33 @@
 output "log_analytics_datasource_windows_performance_counters_id" {
   description = "Map of id values across all log_analytics_datasource_windows_performance_counters, keyed the same as var.log_analytics_datasource_windows_performance_counters"
-  value       = { for k, v in azurerm_log_analytics_datasource_windows_performance_counter.log_analytics_datasource_windows_performance_counters : k => v.id }
+  value       = { for k, v in azurerm_log_analytics_datasource_windows_performance_counter.log_analytics_datasource_windows_performance_counters : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "log_analytics_datasource_windows_performance_counters_counter_name" {
   description = "Map of counter_name values across all log_analytics_datasource_windows_performance_counters, keyed the same as var.log_analytics_datasource_windows_performance_counters"
-  value       = { for k, v in azurerm_log_analytics_datasource_windows_performance_counter.log_analytics_datasource_windows_performance_counters : k => v.counter_name }
+  value       = { for k, v in azurerm_log_analytics_datasource_windows_performance_counter.log_analytics_datasource_windows_performance_counters : k => v.counter_name if v.counter_name != null && length(v.counter_name) > 0 }
 }
 output "log_analytics_datasource_windows_performance_counters_instance_name" {
   description = "Map of instance_name values across all log_analytics_datasource_windows_performance_counters, keyed the same as var.log_analytics_datasource_windows_performance_counters"
-  value       = { for k, v in azurerm_log_analytics_datasource_windows_performance_counter.log_analytics_datasource_windows_performance_counters : k => v.instance_name }
+  value       = { for k, v in azurerm_log_analytics_datasource_windows_performance_counter.log_analytics_datasource_windows_performance_counters : k => v.instance_name if v.instance_name != null && length(v.instance_name) > 0 }
 }
 output "log_analytics_datasource_windows_performance_counters_interval_seconds" {
   description = "Map of interval_seconds values across all log_analytics_datasource_windows_performance_counters, keyed the same as var.log_analytics_datasource_windows_performance_counters"
-  value       = { for k, v in azurerm_log_analytics_datasource_windows_performance_counter.log_analytics_datasource_windows_performance_counters : k => v.interval_seconds }
+  value       = { for k, v in azurerm_log_analytics_datasource_windows_performance_counter.log_analytics_datasource_windows_performance_counters : k => v.interval_seconds if v.interval_seconds != null }
 }
 output "log_analytics_datasource_windows_performance_counters_name" {
   description = "Map of name values across all log_analytics_datasource_windows_performance_counters, keyed the same as var.log_analytics_datasource_windows_performance_counters"
-  value       = { for k, v in azurerm_log_analytics_datasource_windows_performance_counter.log_analytics_datasource_windows_performance_counters : k => v.name }
+  value       = { for k, v in azurerm_log_analytics_datasource_windows_performance_counter.log_analytics_datasource_windows_performance_counters : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "log_analytics_datasource_windows_performance_counters_object_name" {
   description = "Map of object_name values across all log_analytics_datasource_windows_performance_counters, keyed the same as var.log_analytics_datasource_windows_performance_counters"
-  value       = { for k, v in azurerm_log_analytics_datasource_windows_performance_counter.log_analytics_datasource_windows_performance_counters : k => v.object_name }
+  value       = { for k, v in azurerm_log_analytics_datasource_windows_performance_counter.log_analytics_datasource_windows_performance_counters : k => v.object_name if v.object_name != null && length(v.object_name) > 0 }
 }
 output "log_analytics_datasource_windows_performance_counters_resource_group_name" {
   description = "Map of resource_group_name values across all log_analytics_datasource_windows_performance_counters, keyed the same as var.log_analytics_datasource_windows_performance_counters"
-  value       = { for k, v in azurerm_log_analytics_datasource_windows_performance_counter.log_analytics_datasource_windows_performance_counters : k => v.resource_group_name }
+  value       = { for k, v in azurerm_log_analytics_datasource_windows_performance_counter.log_analytics_datasource_windows_performance_counters : k => v.resource_group_name if v.resource_group_name != null && length(v.resource_group_name) > 0 }
 }
 output "log_analytics_datasource_windows_performance_counters_workspace_name" {
   description = "Map of workspace_name values across all log_analytics_datasource_windows_performance_counters, keyed the same as var.log_analytics_datasource_windows_performance_counters"
-  value       = { for k, v in azurerm_log_analytics_datasource_windows_performance_counter.log_analytics_datasource_windows_performance_counters : k => v.workspace_name }
+  value       = { for k, v in azurerm_log_analytics_datasource_windows_performance_counter.log_analytics_datasource_windows_performance_counters : k => v.workspace_name if v.workspace_name != null && length(v.workspace_name) > 0 }
 }
 
